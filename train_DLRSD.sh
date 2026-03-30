91f6698cff1e7756669cdcfda8c4880c059daaf6
xtxdsuers <18373287065@163.com>
2026-03-30 14:02:25 +0800

    Mar 30

--- train_DLRSD.sh
+++ train_DLRSD.sh
@@ -1,5 +1,5 @@
 python train_net.py --config-file configs/vitl_336_DLRSD.yaml --num-gpus 4 \
-    OUTPUT_DIR ./output_paper_DLRSD_test \
+    OUTPUT_DIR ./output_paper_DLRSD_rotatte \
     SOLVER.MAX_ITER 100000 \
     SOLVER.WEIGHT_DECAY 0.001 \
     INPUT.MIN_SIZE_TEST 384 \
